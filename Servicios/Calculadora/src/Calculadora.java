fca878ea3a5b9a96393e2270e6c279666249b56b
RoiMP <roimartinezportela@gmail.com>
2019-04-19 16:41:52 +0200

    Updated from eclipse

---
+++ Servicios/Calculadora/src/Calculadora.java
@@ -0,0 +1,19 @@
+public class Calculadora {
+
+	public float sumar(float s1) {
+		return (s1 + s1);
+	}
+
+	public float restar(float min, float sus) {
+		return (min - sus);
+	}
+
+	public float multiplicar(float m1) {
+		return (m1 * m1);
+	}
+
+	public float dividir(float num, float den) {
+		return (num /den);
+	}
+	
+}
